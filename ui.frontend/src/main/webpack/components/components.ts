67550b4f92cf1b6ad1553212a40d0c5ca8093425
Josh Zeltman <joshua.zeltman@gmail.com>
2020-10-14 12:10:02 -0300

    styling for teaser

---
+++ ui.frontend/src/main/webpack/components/components.ts
@@ -0,0 +1,1 @@
+import './teaser/style/card.ts';
--- ui.frontend/src/main/webpack/site/main.ts
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -1,6 +1,1 @@
-// Stylesheets
-// import "./main.css";
-
-// Javascript or Typescript
-import "./**/*.js";
-import "./**/*.ts";
+import '../components/components.ts';
