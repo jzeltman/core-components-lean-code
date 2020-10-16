85f480fff41ef460b8426ff2e3eeea7562b545c4
Josh Zeltman <joshua.zeltman@gmail.com>
2020-10-16 18:51:52 -0300

    styling for text and title

--- ui.frontend/src/main/webpack/components/languagenavigation/languagenavigation.ts
+++ ui.frontend/src/main/webpack/components/languagenavigation/languagenavigation.ts
@@ -1,13 +1,18 @@
 export default {
   selector: '.cmp-languagenavigation',
   init: (el) => {
-    const languageListEl = el.querySelector('.cmp-languagenavigation__group');
-    const buttonToggleEl = document.createElement('button');
+    const languageListEl: HTMLElement = el.querySelector('.cmp-languagenavigation__group');
+    const buttonToggleEl: HTMLButtonElement = document.createElement('button');
 
     const toggleList = () => languageListEl.classList.toggle('block');
 
     buttonToggleEl.classList.add('cmp-languagenavigation__toggle');
     buttonToggleEl.addEventListener('click', toggleList);
+    document.addEventListener('click', ({ target }: any) => {
+      if (!buttonToggleEl.contains(target) || languageListEl.contains(target)) {
+        languageListEl.classList.remove('block');
+      }
+    });
 
     el.insertBefore(buttonToggleEl, languageListEl);
   }
