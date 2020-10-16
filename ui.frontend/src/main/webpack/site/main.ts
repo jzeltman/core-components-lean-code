4e8ff84e983a8d3a891b4ff5fdf5553078fac7a1
Josh Zeltman <joshua.zeltman@gmail.com>
2020-10-16 17:08:20 -0300

    Styling for the header

--- ui.frontend/src/main/webpack/site/main.ts
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -2,6 +2,14 @@ import './main.css';
 import Components from '../components/components';
 
 const onDocumentReady = () => {
+
+  const headerEl: HTMLElement = document.querySelector('header.experiencefragment');
+  const mainEl: HTMLElement = document.querySelector('.container.--main-behind-header');
+  if (mainEl) {
+    headerEl.classList.add('--main-behind-header');
+    mainEl.style.marginTop = `-${headerEl.offsetHeight}px`;
+  }
+
   Components.forEach(component => {
     const elements = document.querySelectorAll(component.selector);
     for (var i = 0; i < elements.length; i++) {
